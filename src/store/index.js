e4fce8cfaae5ab8bc660c53e24b8cdf329dd890f
Dzianis Dashkevich <dskecse@gmail.com>
2016-06-21 13:57:47 +0300

    Use redux-logger middleware rather than our own logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from '../reducer'
 import randomId from '../middlewares/randomId'
-import logger from '../middlewares/logger'
+// import logger from '../middlewares/logger'
 // import thunk from 'redux-thunk'
 import client from "../middlewares/client"
+import createLogger from "redux-logger"
 
+const logger = createLogger()
 const enhancer = compose(
     applyMiddleware(randomId, client, logger),
     window.devToolsExtension ? window.devToolsExtension() : f => f
